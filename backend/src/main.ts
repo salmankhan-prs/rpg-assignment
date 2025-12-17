d37f3f3763a9edcf617de709e6e70537dd7ce32d
Gaurav <kpguru20001@gmail.com>
2025-12-17 13:27:10 +0530

    Add GraphQL endpoint logging in main.ts for easier debugging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3200);
+  console.log(`Graphql Endpoint: http://localhost:${process.env.PORT ?? 3200}/graphql`);
 }
 bootstrap();
